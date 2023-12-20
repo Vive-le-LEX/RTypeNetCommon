06e5f8faab4c15f01665cb92fb5eb52e18ea87fb
Antoine Frankel <antoine.frankel@epitech.eu>
2023-12-20 13:27:21 +0100

    [~] Updated cmake structure

---
+++ external/FindAsio.cmake
@@ -0,0 +1,26 @@
+find_library(ASIO_LIB asio)
+
+if(NOT ASIO_LIB)
+    message(STATUS "Asio library not found, downloading from github")
+    FetchContent_Declare(
+        asio
+        GIT_REPOSITORY git@github.com:chriskohlhoff/asio.git
+        GIT_TAG master
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+    )
+
+    FetchContent_GetProperties(asio)
+
+    if(NOT asio_POPULATED)
+        FetchContent_Populate(asio)
+    endif()
+
+    add_library(asio INTERFACE)
+
+    target_include_directories(asio INTERFACE ${asio_SOURCE_DIR}/asio/include)
+    find_package(Threads)
+    target_link_libraries(asio INTERFACE Threads::Threads)
+else ()
+    message(STATUS "Asio library found at ${ASIO_LIB}")
+endif()
---
+++ external/FindStduuid.cmake
@@ -0,0 +1,24 @@
+find_library(STDUUID_LIB stduuid)
+
+if(NOT STDUUID_LIB)
+    FetchContent_Declare(
+        stduuid
+        GIT_REPOSITORY git@github.com:mariusbancila/stduuid.git
+        GIT_TAG master
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+    )
+
+    FetchContent_GetProperties(stduuid)
+
+    if(NOT stduuid_POPULATED)
+        FetchContent_Populate(stduuid)
+    endif()
+
+    add_library(stduuid INTERFACE)
+
+    target_include_directories(stduuid INTERFACE ${stduuid_SOURCE_DIR}/include)
+    target_include_directories(stduuid INTERFACE ${stduuid_SOURCE_DIR}/gsl)
+else()
+    message(STATUS "Stduuid library found at ${STDUUID_LIB}")
+endif()
