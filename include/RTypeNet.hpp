7c365c4b604b7e04769f98ee15be05c60ce71113
Viltard Léo <leo.viltard@epitech.eu>
2024-01-13 21:36:25 +0100

    [+] ReAdd Color in JoinLobby

--- include/RTypeNet.hpp
+++ include/RTypeNet.hpp
@@ -226,6 +226,7 @@ namespace RType {
             uuids::uuid lobbyUuid;   ///< The uuid of the lobby
             uuids::uuid clientUuid;  ///< The uuid of the client
             Username_t username;     ///< The username of the client
+            ShipColor color;         ///< The color of the ship
         } JoinLobby_t;
 
         /// @private
